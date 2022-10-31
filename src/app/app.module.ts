ff54e93801a67095813a89af4fb3ccda185b18af
abhay.parsaniya <abhay.parsaniya@bacancy.com>
2022-10-31 22:17:38 +0530

    Basic usage of Component implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainBodyComponent } from './main-body/main-body.component';
+import { FooterComponent } from './footer/footer.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainBodyComponent,
+    FooterComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
